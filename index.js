c19d739830b798f2d1ac3879a7db4edccd553307
tarunpahade <103200889+tarunpahade@users.noreply.github.com>
2022-09-30 16:37:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,751 @@
+
+
+
+
+
+
+  const  dominos=[
+        {
+      
+            price:'69',
+            pizza:'onion'
+        },
+        {
+    
+            price:'99',
+            pizza:'Margherita',
+          
+        },
+        {
+     
+            price:'215',
+            pizza:'farmhouse'
+        },
+        {
+    
+            price:'165',
+            pizza:'Cheese & Corn'
+        },
+        {
+    
+            price:'215',
+            pizza:'Veggie Paradise'
+        },
+        {
+           
+            price:'215',
+            pizza:'peppy paneer'
+        }
+    ]
+ 
+  const  pizzaHut=[
+        {
+            place:'Pizza Hut',
+            price:'230',
+            pizza:'tandoori paneer'
+        },
+        {
+            place:'Pizza Hut',
+            price:'419',
+            pizza:'spicy paneer pizza'
+        }
+        ,
+        {
+            place:'Pizza Hut',
+            price:'529',
+            pizza:'veggie lover'
+        }
+        ,
+        {
+            place:'Pizza Hut',
+            price:'279',
+            pizza:'classic corn'
+        }
+        ,
+        {
+            place:'Pizza Hut',
+            price:'569',
+            pizza:'veggie italian'
+        }
+        ,
+        {
+            place:'Pizza Hut',
+            price:'529',
+            pizza:'farmers pick'
+        }
+    ]
+   
+
+const bistro=[
+        {
+            place:'Bistro',
+            price:'200',
+            pizza:'farm fresh'
+        },
+        {
+            place:'Bistro',
+            price:'350',
+            pizza:'greek'
+        }
+        ,
+        {
+            place:'Bistro',
+            price:'270',
+            pizza:'Paneer tikka'
+        },
+        {
+            place:'Bistro',
+            price:'270',
+            pizza:'BBQ paneer tikka'
+        },
+        {
+            place:'Bistro',
+            price:'270',
+            pizza:'peri peri paneer'
+        },
+    ]
+
+  const  symphony=[
+
+        {
+            place:'Symphony',
+            price:'250',
+            pizza:'Veggie supreme'
+        },
+        {
+            place:'Symphony',
+            price:'175',
+            pizza:'Margherita'
+        },
+        {
+            place:'Symphony',
+            price:'225',
+            pizza:'Fiama'
+        },
+        {
+            place:'Symphony',
+            price:'225',
+            pizza:'Mariana'
+        },
+        {
+            place:'Symphony',
+            price:'250',
+            pizza:'Mexicano'
+        }
+        ,
+        {
+            place:'Symphony',
+            price:'250',
+            pizza:'Paneer BBQ'
+        }
+    ]
+    
+ const  coneCAKES=[
+        {
+            place:'Cone & Cakes',
+            price:'120',
+            pizza:'Cheese pizza'
+        },
+            {
+            place:'Cone & Cakes',
+            price:'165',
+            pizza:'Paneer fantasy'
+        },
+        {
+        place:'Cone & Cakes',
+        price:'135',
+        pizza:'Double Cheese pizza'
+    }
+    ]
+     
+   const teamo=[
+        {
+            place:'Teamo',
+            price:'384',
+            pizza:'classic Marghrita'
+            },
+            {
+            place:'Teamo',
+            price:'408',
+            pizza:'italian pesto pizza'
+            },
+            {
+            place:'Teamo',
+            price:'384',
+            pizza:'onion corn & capcicum'
+            },
+            {
+            place:'Teamo',
+            price:'408',
+            pizza:'Tandoori vegetable'
+            },
+            {
+            place:'Teamo',
+            price:'408',
+            pizza:'American BBQ'
+            },
+    ]
+const pizzawala=[
+    {
+        place:'Pizzawala',
+        price:'270',
+        pizza:'peri peri paneer'
+        },
+        {
+        place:'Pizzawala',
+        price:'220',
+        pizza:'Desi pizza'
+        },
+        {
+        place:'Pizzawala',
+        price:'250',
+        pizza:'Farm Fresh'
+        },
+        {
+        place:'Pizzawala',
+        price:'270',
+        pizza:'Roma'
+        },
+        {
+        place:'Pizzawala',
+        price:'220',
+        pizza:'Shroom'
+        },
+        {
+        place:'Pizzawala',
+        price:'250',
+        pizza:'Veg Supreme'
+        },]
+
+
+       const blush= [  {
+            place:'Cafe Blush',
+            price:'150',
+            pizza:'Golden Corn'
+            },
+            {
+            place:'Cafe Blush',
+            price:'150',
+            pizza:'Bell pepers pizza'
+            },
+            {
+            place:'Cafe Blush',
+            price:'200',
+            pizza:'Peppy pizza'
+            },
+            {
+            place:'Cafe Blush',
+            price:'200',
+            pizza:'China Town Pizza'
+            },
+            {
+            place:'Cafe Blush',
+            price:'150',
+            pizza:'Golden Corn'
+            },
+            {
+            place:'Cafe Blush',
+            price:'250',
+            pizza:'Exotica'
+            },
+            {
+            place:'Cafe Blush',
+            price:'300',
+            pizza:'Mexican'
+            }
+        ]
+
+   
+
+
+  
+
+const McDonalds=[
+    {
+        place:'',
+        price:'55',
+        pizza:'Aloo Tikki Burger'
+    },
+    {
+        place:'McDonalds',
+        price:'110',
+        pizza:'Mc Veggie'
+    },
+    {
+        place:'McDonalds',
+        price:'181',
+        pizza:'Mc Spicy Paneer'
+    },
+    {
+        place:'McDonalds',
+        price:'185',
+        pizza:'Maharaja Mac'
+    },
+    {
+        place:'McDonalds',
+        price:'81',
+        pizza:'Veg Muffin'
+    },
+    {
+        place:'McDonalds',
+        price:'58',
+        pizza:'Masala Grilly Veg'
+    },
+  
+  
+  
+]
+ const Burgerking=[ 
+  
+    {
+        place:'Burger King',
+        price:'58',
+        pizza:'Crispy Veg Wrap'
+    }, 
+     {
+        place:'Burger King',
+        price:'45',
+        pizza:'Crispy Veg Burger'
+    },
+    {
+        place:'Burger King',
+        price:'50',
+        pizza:'Veg Makhani'
+    },
+    {
+        place:'Burger King',
+        price:'99',
+        pizza:'Whopper Jr'
+    },
+    {
+        place:'Burger King',
+        price:'69',
+        pizza:'Classic veg'
+    },
+    {
+        place:'Burger King',
+        price:'50',
+        pizza:'Tikki twist'
+    },
+  
+ ]
+  const Globurg=[
+  
+    {
+        place:'Globurg',
+        price:'220',
+        pizza:'Mr India burger'
+    },
+    {
+        place:'Globurg',
+        price:'240',
+        pizza:'Ma ma mia'
+    },
+    {
+        place:'Globurg',
+        price:'280',
+        pizza:'American Dream'
+    },
+ 
+]
+ 
+ const theGhostKitchen=[
+    {
+        place:'The Ghost Kitchen',
+        price:'289',
+        pizza:'Scramble Drain Brains'
+    },
+       {
+        place:'The Ghost Kitchen',
+        price:'279',
+        pizza:'Crazy Zombieurger'
+    },
+    {
+        place:'The Ghost Kitchen',
+        price:'239',
+        pizza:'The Ghost Grub burger'
+    },
+    {
+        place:'The Ghost Kitchen',
+        price:'309',
+        pizza:'Exotica Paneer'
+    },
+    {
+        place:'The Ghost Kitchen',
+        price:'179',
+        pizza:'The elf'
+    },
+
+]
+const UFO=[
+    {
+        place:'',
+        price:'90',
+        pizza:'Classic Cheese Max fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'110',
+        pizza:'Peri Peri Cheese max fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'130',
+        pizza:'Jalepano Cheese Max fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'120',
+        pizza:'BBQ max fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'160',
+        pizza:'Smokey Fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'180',
+        pizza:'Alien Fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'130',
+        pizza:'Mexican Salsa fries'
+    },
+    {
+        place:'UFO Fries and Corn',
+        price:'130',
+        pizza:'thousand island fries'
+    },
+  
+  
+]
+ const yumwich=[ 
+  
+    {
+        place:'Yumwich',
+        price:'95',
+        pizza:'Cheese fries'
+    },
+    {
+        place:'Yumwich',
+        price:'190',
+        pizza:'Large peri peri fries'
+    },
+    {
+        place:'Yumwich',
+        price:'260',
+        pizza:'larger peri peri open fries'
+    },
+  
+  
+]
+  
+ const blushfries=[ 
+    {
+        place:'cafe blush',
+        price:'100',
+        pizza:'Masala Fries'
+    },
+    {
+        place:'cafe blush',
+        price:'110',
+        pizza:'Tandoori fries'
+    },
+    {
+        place:'cafe blush',
+        price:'110',
+        pizza:'Creemy masala fries'
+    },
+    {
+        place:'cafe blush',
+        price:'130',
+        pizza:'peri peri fries'
+    },
+    {
+        place:'cafe blush',
+        price:'150',
+        pizza:'schezwan stir fry fries'
+    },
+  
+]
+  
+  
+ const symphonyfries=[ 
+    {
+        place:'Symphony',
+        price:'125',
+        pizza:'chips and dips'
+    },
+
+    {
+        place:'Symphony',
+        price:'130',
+        pizza:'piri piri fries'
+    },
+    {
+        place:'Symphony',
+        price:'150',
+        pizza:'Cheesy'
+    },
+]
+
+const gnp=[
+    {
+        place:'GNP',
+        price:'70',
+        pizza:'Salted'
+    },
+    {
+        place:'GNP',
+        price:'80',
+        pizza:'Peri Peri'
+    },
+    {
+        place:'GNP',
+        price:'90',
+        pizza:'Peri peri with dips'
+    },
+    {
+        place:'GNP',
+        price:'80',
+        pizza:'Veggie with fries'
+    },
+   
+] 
+   
+   
+   
+   const mcdfries=[
+   
+    {
+        place:'Mcdonalds',
+        price:'70',
+        pizza:'Fries (small)'
+    },
+    {
+        place:'Mcdonalds',
+        price:'110',
+        pizza:'peri peri fries'
+    },
+    {
+        place:'Mcdonalds',
+        price:'135',
+        pizza:'Mexican cheesy fries'
+    },
+    {
+        place:'Mcdonalds',
+        price:'70',
+        pizza:'Masala Wedges'
+    },
+    {
+        place:'Mcdonalds',
+        price:'135',
+        pizza:'loaded masala wedges'
+    },
+    
+]
+const biryani=[
+    {
+        place:'Dominos',
+        price:'69',
+        pizza:'onion'
+    },
+    {
+        place:'Oven Story',
+        price:'169',
+        pizza:'Margherita',
+        Time:'47 min'
+    },
+    {
+        place:'pizzawala',
+        price:'269',
+        pizza:'veg loaded'
+    }
+
+]
+
+const pasta=[
+    {
+        place:'Dominos',
+        price:'69',
+        pizza:'onion'
+    },
+    {
+        place:'Oven Story',
+        price:'169',
+        pizza:'Margherita',
+        Time:'47 min'
+    },
+    {
+        place:'pizzawala',
+        price:'269',
+        pizza:'veg loaded'
+    }
+
+]
+//functions
+
+function lulli(lulli){
+    const div=document.createElement('div')
+    const title=document.createElement('h3')
+    const p=document.createElement('p')
+    const alltext=document.createElement('div')
+                            
+    div.classList.add('card')
+    title.classList.add('title')
+    p.classList.add('pricing')
+                                    
+                                
+    div.innerHTML=lulli.place
+    title.innerHTML=lulli.pizza
+    p.innerHTML=lulli.price
+                                
+    alltext.append(title,p)
+    div.append(alltext)
+                                
+   const cord= document.querySelector('.card-group')
+   cord.append(div)
+}
+
+function lulli2(lulli2){
+    const cardss=document.createElement('div')
+    const card1=document.createElement('div')
+    const cardtxt=document.createElement('div')
+    const h2=document.createElement('h3')
+    const price=document.createElement('p')
+    const h1=document.createElement('h1')
+    
+    cardss.classList.add('cardss')
+    card1.classList.add('card1')
+    cardtxt.classList.add('card-txt')
+    h2.classList.add('h2')
+    price.classList.add('price')
+    h1.classList.add('heading')
+    
+ 
+    h2.innerText=lulli2.pizza
+    price.innerText=lulli2.price
+    
+    
+    cardtxt.append(h2,price)
+    card1.append(cardtxt)
+    cardss.append(card1)
+    
+    const cord= document.querySelector('body > div:last-of-type')
+       cord.append(cardss)   
+      
+}
+
+
+
+function place(pk){
+    const headline=  document.createElement('h1')
+ 
+    headline.innerHTML=pk
+    headline.classList.add('heading')
+    document.body.append(headline)
+
+const cardgroup=document.createElement('div')
+cardgroup.classList.add('card-group')
+document.body.append(cardgroup)
+}
+
+
+function auto(name){
+    name.forEach(name=>{
+      
+        lulli2(name);
+        })  
+}
+
+// onclick
+document.querySelectorAll('.food-item').forEach(item => {
+    item.onclick = function () {
+
+        
+
+
+
+ if(item.dataset.item==='pizza'){
+place('Dominos')
+auto(dominos)
+
+place('Pizza Hut')
+auto(pizzaHut)
+
+place('Bistro')
+auto(bistro)
+
+place('Symphony')
+auto(symphony)
+
+place('Cone & cakes')
+auto(coneCAKES)
+
+place('Teamo')
+auto(teamo)
+
+place('Pizzawala')
+auto(pizzawala)
+
+place('Cafe Blush')
+auto(blush)
+}
+
+
+                             
+if(item.dataset.item==='burger'){
+place('McDonalds')
+auto(McDonalds)
+              
+place('Burgerking')
+auto(Burgerking)
+
+place('Globurg')
+auto(Globurg)
+
+place('The Ghost Kitchen')
+auto(theGhostKitchen)
+}
+
+if(item.dataset.item==='fries'){
+
+place('UFO Fries and Corn')
+auto(UFO)
+
+place('Yumwich')
+auto(yumwich)
+
+place('blush')
+auto(blushfries)
+
+place('gnp')
+auto(gnp)
+
+place('Symphony')
+auto(symphonyfries)
+
+place('McDonalds')
+auto(mcdfries)
+
+
+
+}
+   
+
+  if(item.dataset.item==='pasta'){
+
+  pasta.forEach(pasta=>{  
+                        lulli(pasta)
+  })
+                                                                        }                                                                        
+ document.querySelector('.food').style.display = 'none'
+}
+})
+
+
+
+
+    
